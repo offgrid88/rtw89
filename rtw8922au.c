854be0f515dd97ca9b1a3f47479090dfda4512ca
John in TPE <et020598@gmail.com>
2026-03-19 13:30:14 +0800

    Add support for Edimax EW-7822UN7 (8912AU)

--- rtw8922au.c
+++ rtw8922au.c
@@ -53,6 +53,8 @@ static const struct usb_device_id rtw_8922au_id_table[] = {
 	  .driver_info = (kernel_ulong_t)&rtw89_8922au_info },
 	{ USB_DEVICE_AND_INTERFACE_INFO(0x7392, 0x3822, 0xff, 0xff, 0xff),
 	  .driver_info = (kernel_ulong_t)&rtw89_8922au_info },
+	{ USB_DEVICE_AND_INTERFACE_INFO(0x7392, 0x4822, 0xff, 0xff, 0xff),
+	  .driver_info = (kernel_ulong_t)&rtw89_8922au_info },
 	{},
 };
 MODULE_DEVICE_TABLE(usb, rtw_8922au_id_table);
